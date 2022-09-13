7762d42f4644206b38547f8c5328639b626e546f
Vy Nguyen <nguyenvy@umich.edu>
2022-09-13 14:37:54 -0400

    Use dictionary to calculate number of total, harmonized variables

--- f - calculate_num_variables.R
+++ f - calculate_num_variables.R
@@ -1,4 +1,5 @@
-calculate_num_variables <- function(list_documentation)
+calculate_num_variables <- function(list_documentation
+                                    , dataset_dictionary)
 {
   names_dataset <- names(list_documentation) %>%
     gsub(" Fix Category$| \\(trimmed for spaces\\)$| \\(old\\)$"
@@ -16,10 +17,10 @@ calculate_num_variables <- function(list_documentation)
     subset_files_i <- list_documentation[[name_dataset_i]] %>%
       select(variable_codename
              , variable_codename_use
+             , file_name
              , year	
              , SDDSRVYR) %>%
       unique(.) %>%
-      drop_na(file_name) %>%
       mutate(in_dataset = name_dataset_i)
   }
   
@@ -27,4 +28,20 @@ calculate_num_variables <- function(list_documentation)
     map(.
         , extract_file_columns) %>%
     bind_rows(.)
+  View(df_files)
+  
+  num_unique_original_variables <- df_files %>%
+    filter(grepl("^WT_", variable_codename) == FALSE) %>%
+    filter(grepl("^VN", variable_codename) == FALSE) %>%
+    drop_na(file_name) %>%
+    pull(variable_codename) %>%
+    unique(.) %>%
+    length(.)
+  print(num_unique_original_variables)
+  
+  num_harmonized_variables <- dataset_dictionary %>%
+    pull(variable_codename_use) %>%
+    unique(.) %>%
+    length(.)
+  print(num_harmonized_variables)
 }
