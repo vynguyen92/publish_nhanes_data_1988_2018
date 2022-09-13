fc62acae75d7958852ab575d839b03eaffa49210
Vy Nguyen <nguyenvy@umich.edu>
2022-09-13 15:09:45 -0400

    Account for comment variables

--- f - calculate_num_variables.R
+++ f - calculate_num_variables.R
@@ -1,6 +1,9 @@
 calculate_num_variables <- function(list_documentation
                                     , dataset_dictionary)
 {
+  library(usefun)
+  library(tidyverse)
+  
   names_dataset <- names(list_documentation) %>%
     gsub(" Fix Category$| \\(trimmed for spaces\\)$| \\(old\\)$"
          , ""
@@ -22,6 +25,29 @@ calculate_num_variables <- function(list_documentation
              , SDDSRVYR) %>%
       unique(.) %>%
       mutate(in_dataset = name_dataset_i)
+    
+    if(name_dataset_i == "Chemicals")
+    {
+      comments_i <- list_documentation[[name_dataset_i]] %>%
+        select(comment_codename
+               , comment_codename_use
+               , file_name
+               , year	
+               , SDDSRVYR) %>%
+        drop_na(comment_codename_use) %>%
+        unique(.) %>%
+        rename(variable_codename = comment_codename) %>%
+        rename(variable_codename_use = comment_codename_use) %>%
+        mutate(in_dataset = "Comments")
+      
+      subset_files_i <- subset_files_i %>%
+        full_join(.
+                  , comments_i
+                  , by = colnames(.))
+        
+    }
+    
+    return(subset_files_i)
   }
   
   df_files <- names_dataset %>%
@@ -39,9 +65,17 @@ calculate_num_variables <- function(list_documentation
     length(.)
   print(num_unique_original_variables)
   
-  num_harmonized_variables <- dataset_dictionary %>%
+  harmonized_variables_dictionary <- dataset_dictionary %>%
     pull(variable_codename_use) %>%
-    unique(.) %>%
-    length(.)
-  print(num_harmonized_variables)
+    unique(.) 
+  print(length(harmonized_variables_dictionary))
+  
+  harmonized_df_files <- df_files %>%
+    pull(variable_codename_use) %>%
+    unique(.) 
+  print(length(harmonized_df_files))
+    
+  print(outersect(harmonized_variables_dictionary
+                  , harmonized_df_files))
+  # print(num_harmonized_variables)
 }
