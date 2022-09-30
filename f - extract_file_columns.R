d84f33401a883256c5d31855430e5eaeecd2cd99
Vy Nguyen <nguyenvy@umich.edu>
2022-09-30 11:51:28 -0400

    Create f - extract_file_columns.R

---
+++ f - extract_file_columns.R
@@ -0,0 +1,39 @@
+extract_file_columns <- function(x)
+{
+  name_dataset_i <- x
+  
+  subset_files_i <- list_documentations[[name_dataset_i]] %>%
+    select(variable_codename_use
+           , variable_description_use
+           , file_name
+           , file_category
+           , year	
+           , SDDSRVYR) %>%
+    unique(.) %>%
+    mutate(in_dataset = str_to_title(name_dataset_i))
+  
+  if(name_dataset_i == "Chemicals")
+  {
+    comments_i <- list_documentations[[name_dataset_i]] %>%
+      select(comment_codename_use
+             , variable_description_use
+             , units
+             , file_name
+             , file_category
+             , year	
+             , SDDSRVYR) %>%
+      drop_na(comment_codename_use) %>%
+      unique(.) %>%
+      rename(variable_codename_use = comment_codename_use) %>%
+      mutate(in_dataset = "Comments")
+    # View(comments_i)
+    
+    subset_files_i <- subset_files_i %>%
+      full_join(.
+                , comments_i
+                , by = colnames(.))
+    
+  }
+  
+  return(subset_files_i)
+}
