4be346a95f080acc9cafcce1f9f45d5d622ed35b
Vy Nguyen <nguyenvy@umich.edu>
2022-09-30 14:34:22 -0400

    Create f - creat_dictionary_harmonized_categories.R

---
+++ f - creat_dictionary_harmonized_categories.R
@@ -0,0 +1,6 @@
+create_dictionary_harmonized_categories <- function(list_documentations)
+{
+  names_doc_cleaning <- c("Demographics Fix Category"
+                          , "Questionnaire Fix Category"
+                          , "Response Fix Category")
+}
