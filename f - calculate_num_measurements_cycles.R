9e2343a424cdc4b7b4320db022563fea728d33ad
Vy Nguyen <nguyenvy@umich.edu>
2022-09-30 14:31:11 -0400

    Calculate number of participants with measurements

--- f - calculate_num_measurements_cycles.R
+++ f - calculate_num_measurements_cycles.R
@@ -5,6 +5,8 @@ calculate_num_measurements_cycles <- function(x
   
   vector_cycles <- df_nhanes[,"SDDSRVYR"]
   
+  SEQN_new <- df_nhanes[,"SEQN_new"]
+  
   # print(colnames(x))
   
   df_cycles_labels <- data.frame(cycles = c(-1, seq(10))
@@ -22,16 +24,22 @@ calculate_num_measurements_cycles <- function(x
                                  , stringsAsFactors = FALSE) 
   # View(df_cycles_labels)
   
-  num_measurements <- sum(is.na(x) == FALSE) 
+  # num_participants_measurements <- sum(is.na(x) == FALSE) 
   # print(num_measurements)
   
-  df_variable_cycle <- data.frame(variable_i = x
+  df_variable_cycle <- data.frame(SEQN_new = SEQN_new
+                                  , variable_i = x
                                   , cycles = vector_cycles
                                   , stringsAsFactors = FALSE) %>%
     na.omit(.) %>%
     left_join(.
               , df_cycles_labels
               , by = "cycles")
+  
+  num_participants_measurements <- df_variable_cycle %>%
+    pull(SEQN_new) %>%
+    unique(.) %>%
+    length(.)
   
   unique_cycles <- df_variable_cycle %>%
     pull(cycle_labels) %>%
@@ -41,7 +49,7 @@ calculate_num_measurements_cycles <- function(x
           , collapse = ", ")
   # print(unique_cycles)
   
-  df_stats <- data.frame(num_measurements = num_measurements
+  df_stats <- data.frame(num_participants_measurements = num_participants_measurements
                          , unique_cycles = unique_cycles
                          , stringsAsFactors = FALSE)
   
