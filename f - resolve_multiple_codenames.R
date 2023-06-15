b5459a4829dcba3df6f47e32e25c1177b2ce8291
Vy Nguyen <nguyenvy@umich.edu>
2023-06-15 14:35:35 -0400

    Write an if statement on deriving the variables

--- f - resolve_multiple_codenames.R
+++ f - resolve_multiple_codenames.R
@@ -250,63 +250,66 @@ resolve_multiple_codenames <- function(vector_corrected_codenames
   num_codenames_derived <- length(codenames_derived)
   # print(num_codenames_derived)
   
-  for(i in seq(num_codenames_derived))
+  if(num_codenames_derived != 0)
   {
-    corrected_codename_i <- codenames_derived[i]
-    
-    subset_derived_i <- subset_variables_derived %>%
-      filter(variable_codename_use == corrected_codename_i)
-    # print(subset_derived_i)
-
-    unique_cycles <- subset_derived_i %>%
-      pull(SDDSRVYR)
-    # print(unique_cycles)
-
-    index_affected_cycles <- which(df_unclean$SDDSRVYR %in% unique_cycles)
-    # print(unique(df_unclean[index_affected_cycles, "SDDSRVYR"]))
-
-    codename_note_i <- subset_derived_i %>%
-      pull(codename_note) %>%
-      unique(.)
-    # print(codename_note_i)
-
-    codenames_used_for_deriving <- codename_note_i %>%
-      gsub("Derive from ", "", .) %>%
-      strsplit(., split = " and ") %>%
-      unlist(.)
-    # print(codenames_used_for_deriving)
-
-    questionaire_codename <- codenames_used_for_deriving %>%
-      grepl("G$", .) %>%
-      codenames_used_for_deriving[.]
-    # print(questionaire_codename)
-
-    value_codename <- codenames_used_for_deriving %>%
-      grepl("Q$", .) %>%
-      codenames_used_for_deriving[.]
-    # print(value_codename)
-
-    questionaire_variable <- df_unclean[index_affected_cycles, questionaire_codename]
-
-    value_variable <- df_unclean[index_affected_cycles, value_codename]
-
-    # print(unique(df_unclean[index_affected_cycles,corrected_codename_i]))
-    df_unclean[index_affected_cycles,corrected_codename_i] <- questionaire_variable
-    # print(unique(df_unclean[index_affected_cycles,corrected_codename_i]))
-
-    
-
-    index_questionaire_variable_is_1 <- which(df_unclean[,corrected_codename_i] == 1 & df_unclean$SDDSRVYR %in% unique_cycles)
-    # print(unique(df_unclean[index_questionaire_variable_is_1,corrected_codename_i]))
-
-    df_unclean[index_questionaire_variable_is_1,corrected_codename_i] <- df_unclean[index_questionaire_variable_is_1,value_codename]
-    
-    # # For checking
-    # View(df_unclean[index_affected_cycles,c(corrected_codename_i
-    #                                         , questionaire_codename
-    #                                         , value_codename
-    #                                         , "SDDSRVYR")] %>%
-    #        unique(.))
+    for(i in seq(num_codenames_derived))
+    {
+      corrected_codename_i <- codenames_derived[i]
+      
+      subset_derived_i <- subset_variables_derived %>%
+        filter(variable_codename_use == corrected_codename_i)
+      # print(subset_derived_i)
+      
+      unique_cycles <- subset_derived_i %>%
+        pull(SDDSRVYR)
+      # print(unique_cycles)
+      
+      index_affected_cycles <- which(df_unclean$SDDSRVYR %in% unique_cycles)
+      # print(unique(df_unclean[index_affected_cycles, "SDDSRVYR"]))
+      
+      codename_note_i <- subset_derived_i %>%
+        pull(codename_note) %>%
+        unique(.)
+      # print(codename_note_i)
+      
+      codenames_used_for_deriving <- codename_note_i %>%
+        gsub("Derive from ", "", .) %>%
+        strsplit(., split = " and ") %>%
+        unlist(.)
+      # print(codenames_used_for_deriving)
+      
+      questionaire_codename <- codenames_used_for_deriving %>%
+        grepl("G$", .) %>%
+        codenames_used_for_deriving[.]
+      # print(questionaire_codename)
+      
+      value_codename <- codenames_used_for_deriving %>%
+        grepl("Q$", .) %>%
+        codenames_used_for_deriving[.]
+      # print(value_codename)
+      
+      questionaire_variable <- df_unclean[index_affected_cycles, questionaire_codename]
+      
+      value_variable <- df_unclean[index_affected_cycles, value_codename]
+      
+      # print(unique(df_unclean[index_affected_cycles,corrected_codename_i]))
+      df_unclean[index_affected_cycles,corrected_codename_i] <- questionaire_variable
+      # print(unique(df_unclean[index_affected_cycles,corrected_codename_i]))
+      
+      
+      
+      index_questionaire_variable_is_1 <- which(df_unclean[,corrected_codename_i] == 1 & df_unclean$SDDSRVYR %in% unique_cycles)
+      # print(unique(df_unclean[index_questionaire_variable_is_1,corrected_codename_i]))
+      
+      df_unclean[index_questionaire_variable_is_1,corrected_codename_i] <- df_unclean[index_questionaire_variable_is_1,value_codename]
+      
+      # # For checking
+      # View(df_unclean[index_affected_cycles,c(corrected_codename_i
+      #                                         , questionaire_codename
+      #                                         , value_codename
+      #                                         , "SDDSRVYR")] %>%
+      #        unique(.))
+    }
   }
   
   # Define the dataset as cleaned with all the codename harmonized
